643254f92f16b4a4458242077455d48ea0351c74
data2 <muskteerathos@gmail.com>
2023-04-07 11:51:09 +0800

    服装

---
+++ 基本分类：对服装图像进行分类/demo.py
@@ -0,0 +1,149 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+
+# 构建模型
+# 构建神经网络需要先配置模型的层，然后再编译模型。
+
+# 设置层
+# 神经网络的基本组成部分是层。层会从向其馈送的数据中提取表示形式。希望这些表示形式有助于解决手头上的问题。
+
+# 大多数深度学习都包括将简单的层链接在一起。大多数层（如 tf.keras.layers.Dense）都具有在训练期间才会学习的参数。
+
+
+
+# 该网络的第一层 tf.keras.layers.Flatten 将图像格式从二维数组（28 x 28 像素）转换成一维数组（28 x 28 = 784 像素）。将该层视为图像中未堆叠的像素行并将其排列起来。该层没有要学习的参数，它只会重新格式化数据。
+
+# 展平像素后，网络会包括两个 tf.keras.layers.Dense 层的序列。它们是密集连接或全连接神经层。第一个 Dense 层有 128 个节点（或神经元）。第二个（也是最后一个）层会返回一个长度为 10 的 logits 数组。每个节点都包含一个得分，用来表示当前图像属于 10 个类中的哪一类。
+
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+
+# 编译模型
+# 在准备对模型进行训练之前，还需要再对其进行一些设置。以下内容是在模型的编译步骤中添加的：
+
+# 损失函数 - 测量模型在训练期间的准确程度。你希望最小化此函数，以便将模型“引导”到正确的方向上。
+# 优化器 - 决定模型如何根据其看到的数据和自身的损失函数进行更新。
+# 指标 - 用于监控训练和测试步骤。以下示例使用了准确率，即被正确分类的图像的比率。
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# 训练模型
+# 训练神经网络模型需要执行以下步骤：
+
+# 将训练数据馈送给模型。在本例中，训练数据位于 train_images 和 train_labels 数组中。
+# 模型学习将图像和标签关联起来。
+# 要求模型对测试集（在本例中为 test_images 数组）进行预测。
+# 验证预测是否与 test_labels 数组中的标签相匹配。
+# 向模型馈送数据
+# 要开始训练，请调用 model.fit 方法，这样命名是因为该方法会将模型与训练数据进行“拟合”：
+
+model.fit(train_images, train_labels, epochs=10)
+
+
+# 评估准确率
+# 接下来，比较模型在测试数据集上的表现：
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+# 结果表明，模型在测试数据集上的准确率略低于训练数据集。训练准确率和测试准确率之间的差距代表过拟合。过拟合是指机器学习模型在新的、以前未曾见过的输入上的表现不如在训练数据上的表现。过拟合的模型会“记住”训练数据集中的噪声和细节，从而对模型在新数据上的表现产生负面影响。有关更多信息，请参阅以下内容：
+
+
+# 进行预测
+# 模型经过训练后，您可以使用它对一些图像进行预测。附加一个 Softmax 层，将模型的线性输出 logits 转换成更容易理解的概率。
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+# 在上例中，模型预测了测试集中每个图像的标签。我们来看看第一个预测结果：
+predictions[0]
+
+
+# 预测结果是一个包含 10 个数字的数组。它们代表模型对 10 种不同服装中每种服装的“置信度”。您可以看到哪个标签的置信度值最大：
+
+
+np.argmax(predictions[0])
+
+
+# # 您可以将其绘制成图表，看看模型对于全部 10 个类的预测。
+
+
+# def plot_image(i, predictions_array, true_label, img):
+#   true_label, img = true_label[i], img[i]
+#   plt.grid(False)
+#   plt.xticks([])
+#   plt.yticks([])
+
+#   plt.imshow(img, cmap=plt.cm.binary)
+
+#   predicted_label = np.argmax(predictions_array)
+#   if predicted_label == true_label:
+#     color = 'blue'
+#   else:
+#     color = 'red'
+
+#   plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+#                                 100*np.max(predictions_array),
+#                                 class_names[true_label]),
+#                                 color=color)
+
+# def plot_value_array(i, predictions_array, true_label):
+#   true_label = true_label[i]
+#   plt.grid(False)
+#   plt.xticks(range(10))
+#   plt.yticks([])
+#   thisplot = plt.bar(range(10), predictions_array, color="#777777")
+#   plt.ylim([0, 1])
+#   predicted_label = np.argmax(predictions_array)
+
+#   thisplot[predicted_label].set_color('red')
+#   thisplot[true_label].set_color('blue')
+
+# # 验证预测结果
+# # 在模型经过训练后，您可以使用它对一些图像进行预测。
+
+# # 让我们用模型的预测绘制几张图像。请注意，即使置信度很高，模型也可能出错。
+
+# # Plot the first X test images, their predicted labels, and the true labels.
+# # Color correct predictions in blue and incorrect predictions in red.
+# num_rows = 5
+# num_cols = 3
+# num_images = num_rows*num_cols
+# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+# for i in range(num_images):
+#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
+#   plot_image(i, predictions[i], test_labels, test_images)
+#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
+#   plot_value_array(i, predictions[i], test_labels)
+# plt.tight_layout()
+# plt.show()
