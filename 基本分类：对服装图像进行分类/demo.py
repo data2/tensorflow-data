dba37d0b203b49959b7107d03a799f65b77c5614
data2 <muskteerathos@gmail.com>
2023-04-07 15:41:24 +0800

    1

--- 基本分类：对服装图像进行分类/demo.py
+++ 基本分类：对服装图像进行分类/demo.py
@@ -86,64 +86,87 @@ probability_model = tf.keras.Sequential([model,
 predictions = probability_model.predict(test_images)
 
 # 在上例中，模型预测了测试集中每个图像的标签。我们来看看第一个预测结果：
-predictions[0]
+print(predictions[0])
+
+
 
 
 # 预测结果是一个包含 10 个数字的数组。它们代表模型对 10 种不同服装中每种服装的“置信度”。您可以看到哪个标签的置信度值最大：
 
 
-np.argmax(predictions[0])
+# np.argmax(predictions[0])
 
 
 # # 您可以将其绘制成图表，看看模型对于全部 10 个类的预测。
 
 
-# def plot_image(i, predictions_array, true_label, img):
-#   true_label, img = true_label[i], img[i]
-#   plt.grid(False)
-#   plt.xticks([])
-#   plt.yticks([])
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
 
-#   plt.imshow(img, cmap=plt.cm.binary)
+  plt.imshow(img, cmap=plt.cm.binary)
 
-#   predicted_label = np.argmax(predictions_array)
-#   if predicted_label == true_label:
-#     color = 'blue'
-#   else:
-#     color = 'red'
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
 
-#   plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-#                                 100*np.max(predictions_array),
-#                                 class_names[true_label]),
-#                                 color=color)
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
 
-# def plot_value_array(i, predictions_array, true_label):
-#   true_label = true_label[i]
-#   plt.grid(False)
-#   plt.xticks(range(10))
-#   plt.yticks([])
-#   thisplot = plt.bar(range(10), predictions_array, color="#777777")
-#   plt.ylim([0, 1])
-#   predicted_label = np.argmax(predictions_array)
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
 
-#   thisplot[predicted_label].set_color('red')
-#   thisplot[true_label].set_color('blue')
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 # # 验证预测结果
 # # 在模型经过训练后，您可以使用它对一些图像进行预测。
 
 # # 让我们用模型的预测绘制几张图像。请注意，即使置信度很高，模型也可能出错。
 
-# # Plot the first X test images, their predicted labels, and the true labels.
-# # Color correct predictions in blue and incorrect predictions in red.
-# num_rows = 5
-# num_cols = 3
-# num_images = num_rows*num_cols
-# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-# for i in range(num_images):
-#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
-#   plot_image(i, predictions[i], test_labels, test_images)
-#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
-#   plot_value_array(i, predictions[i], test_labels)
-# plt.tight_layout()
-# plt.show()
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+img = test_images[1]
+
+img = (np.expand_dims(img,0))
+
+predictions_single = probability_model.predict(img)
+
+print(predictions_single)
+
+np.argmax(predictions_single[0])
